2716e36c68928740fb28e487cfbc570474907e41
ShoutaTakahashi <70875306+st18126sh@users.noreply.github.com>
2022-06-16 17:14:57 +0900

    ｂ

--- team4.c
+++ team4.c
@@ -1,8 +1,8 @@
 int team4(int ID,int n,int SC[2] , int *H)/*ID0の関数*/
 {
   srand((unsigned)time(NULL));
-  int random = (rand()%(100 - 0 + 1)) + min;
-  
+  int random = (rand()%(100 - 0 + 1)) + 0;
+  int percent = ;
   int Enemy_Last_Hand = *(h+2*n+ID);
   if(percent < random){
     int My_Next_Hand = Enemy_Last_Hand ;
